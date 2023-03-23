ada03d1b7c46a1022bbf1f5f1e16a8c29e715de5
NeonHayford <hayford.lumorvi@gmail.com>
2023-03-23 16:28:36 +0000

    image transformation & other

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
-const btnoptions = document.querySelector('.btnOption');
-// btnoptions.innerHTML = 'Rent the secret';
-console.log(btnoptions);
+  document.addEventListener('DOMContentLoaded', function(){
+    let btnoption = document.querySelector('.login');
+    let option = document.querySelector('.signup');
+    // console.log(btnoptions);
+    let changed = false;
 
-const create=()=>{
-    const options = document.getElementsByClassName('signBtn');
-    options.style.backgroundColor = 'white';
-    options.style.color = 'black';
-}
+    const changesignup=()=>{
+        option.style.backgroundColor="white";
+        option.style.color = "black";
+    }
+    const changelogin=()=>{
+      btnoption.style.backgroundColor="black";
+      btnoption.style.color = "white";
+  }
+
+    btnoption.addEventListener('click',changelogin);
+    option.addEventListener('click',changesignup);
 
 
-btnoptions.addEventListener('click',create);
+    const images = ["./images/kyle-johnson.jpg", "./images/michael-krahn.jpg", "./images/eberhard-grossgasteiger.jpg", "./images/huper-by-joshua-b.jpg", "./images/huper-by-joshua-a.jpg", "./images/jordan-steranka.jpg",];
+    let index = 1;
+
+    const changeImage1 = () => {
+    const imageContainer1 = document.querySelector("#turn1");
+    imageContainer1.style.backgroundImage = `url(${images[index]})`;
+    // increment index
+    index++;
+    if (index >= images.length) {
+      index = 0;
+    }
+  }
+  const changeImage3 = () => {
+    const imageContainer3 = document.querySelector("#turn3");
+    imageContainer3.style.backgroundImage = `url(${images[index]})`;
+    index++;
+    if (index >= images.length) {
+      index = 0;
+    }
+  }
+  const changeImage2 = () => {
+    const imageContainer2 = document.querySelector("#turn2");
+    imageContainer2.style.backgroundImage = `url(${images[index]})`;
+    // increment index
+    index++;
+    if (index >= images.length) {
+      index = 0;
+    }
+  }
+  setInterval(() => {
+    return changeImage1()
+  }, 5000);
+  setInterval(() => {
+    return changeImage2()
+  }, 10000);
+  setInterval(() => {
+    return changeImage3()
+  }, 15000);
+})
